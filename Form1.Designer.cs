ec35a45e13f7ff600a33df3235a09ebe9e8452df
nireden <nire@artcybe.com>
2023-09-12 11:56:31 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonPTTPush = new Button();
+            buttonPTTRelease = new Button();
+            buttonRead = new Button();
+            textBoxRead = new TextBox();
+            buttonInitialize = new Button();
+            SuspendLayout();
+            // 
+            // buttonPTTPush
+            // 
+            buttonPTTPush.Enabled = false;
+            buttonPTTPush.Location = new Point(92, 41);
+            buttonPTTPush.Name = "buttonPTTPush";
+            buttonPTTPush.Size = new Size(75, 23);
+            buttonPTTPush.TabIndex = 0;
+            buttonPTTPush.Text = "Push";
+            buttonPTTPush.UseVisualStyleBackColor = true;
+            buttonPTTPush.Click += buttonPTTPush_Click;
+            // 
+            // buttonPTTRelease
+            // 
+            buttonPTTRelease.Enabled = false;
+            buttonPTTRelease.Location = new Point(92, 70);
+            buttonPTTRelease.Name = "buttonPTTRelease";
+            buttonPTTRelease.Size = new Size(75, 23);
+            buttonPTTRelease.TabIndex = 1;
+            buttonPTTRelease.Text = "Release";
+            buttonPTTRelease.UseVisualStyleBackColor = true;
+            buttonPTTRelease.Click += buttonPTTRelease_Click;
+            // 
+            // buttonRead
+            // 
+            buttonRead.Enabled = false;
+            buttonRead.Location = new Point(92, 99);
+            buttonRead.Name = "buttonRead";
+            buttonRead.Size = new Size(75, 23);
+            buttonRead.TabIndex = 2;
+            buttonRead.Text = "Read";
+            buttonRead.UseVisualStyleBackColor = true;
+            buttonRead.Click += buttonRead_Click;
+            // 
+            // textBoxRead
+            // 
+            textBoxRead.Location = new Point(31, 128);
+            textBoxRead.Name = "textBoxRead";
+            textBoxRead.Size = new Size(197, 23);
+            textBoxRead.TabIndex = 3;
+            // 
+            // buttonInitialize
+            // 
+            buttonInitialize.Location = new Point(92, 12);
+            buttonInitialize.Name = "buttonInitialize";
+            buttonInitialize.Size = new Size(75, 23);
+            buttonInitialize.TabIndex = 5;
+            buttonInitialize.Text = "Initialize";
+            buttonInitialize.UseVisualStyleBackColor = true;
+            buttonInitialize.Click += buttonInitialize_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(259, 158);
+            Controls.Add(buttonInitialize);
+            Controls.Add(textBoxRead);
+            Controls.Add(buttonRead);
+            Controls.Add(buttonPTTRelease);
+            Controls.Add(buttonPTTPush);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonPTTPush;
+        private Button buttonPTTRelease;
+        private Button buttonRead;
+        private TextBox textBoxRead;
+        private Button buttonEnumarate;
+        private Button buttonInitialize;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+using HidLibrary;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private int soundVendorId, soundProductId;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPTTPush_Click(object sender, EventArgs e)
+        {
+            var device = HidDevices.Enumerate(soundVendorId, soundProductId).FirstOrDefault();
+            device.OpenDevice();
+            var startReport = new HidReport(4);
+            startReport.ReportId = 0x00;
+            startReport.Data = new byte[] { 0x00,
+                0x04, // H/L
+                0x04, // 0=input ; 1=output
+                0x00 };
+            device.WriteReport(startReport);
+            device.CloseDevice();
+        }
+
+        private void buttonPTTRelease_Click(object sender, EventArgs e)
+        {
+            var device = HidDevices.Enumerate(soundVendorId, soundProductId).FirstOrDefault();
+            device.OpenDevice();
+            var stopReport = new HidReport(4);
+            stopReport.ReportId = 0x00;
+            stopReport.Data = new byte[] {  0x00,
+                0x00, // H/L
+                0x04, // 0=input ; 1=output
+                0x00 };
+            device.WriteReport(stopReport);
+            device.CloseDevice();
+        }
+
+        private void buttonRead_Click(object sender, EventArgs e)
+        {
+            textBoxRead.Text = "Waiting for an event";
+            (new Thread(() => {
+                var device = HidDevices.Enumerate(soundVendorId, soundProductId).FirstOrDefault();
+                device.OpenDevice();
+
+                do
+                {
+                    HidReport readReport = device.ReadReport();
+
+                    if (readReport.Exists)
+                    {
+                        // Process the report data
+                        byte[] data = readReport.Data;
+                        textBoxRead.Invoke(() => { textBoxRead.Text = "Received data: " + BitConverter.ToString(data); }) ;
+                    }
+                    
+                } while (true);
+            })).Start();
+
+        }
+
+        private void buttonInitialize_Click(object sender, EventArgs e)
+        {
+            // Enumerate all HID devices
+            var devices = HidDevices.Enumerate();
+            textBoxRead.Text = "No compatible device detected.";
+            foreach (var device in devices)
+            {
+                var vendorId = device.Attributes.VendorId;
+                var productId = device.Attributes.ProductId;
+
+
+                // Check for CM108/108B/109/119/119A/119B
+                if (vendorId == 0x0D8C
+                    && ((productId >= 0x0008 && productId <= 0x000F)
+                        || productId == 0x0012
+                        || productId == 0x013A
+                        || productId == 0x013C
+                        || productId == 0x0013))
+                {
+                    textBoxRead.Text = "CM108 compatible device detected.";
+                    soundProductId = productId;
+                    soundVendorId = vendorId;
+                    buttonPTTPush.Enabled = true;
+                    buttonPTTRelease.Enabled = true;
+                    buttonRead.Enabled = true;
+                }
+                // Check for SSS1621/23
+                else if (vendorId == 0x0C76
+                         && (productId == 0x1605
+                             || productId == 0x1607
+                             || productId == 0x160B))
+                {
+                    textBoxRead.Text = "SSS1621/23 compatible device detected.";
+                    soundProductId = productId;
+                    soundVendorId = vendorId;
+                    buttonPTTPush.Enabled = true;
+                    buttonPTTRelease.Enabled = true;
+                    buttonRead.Enabled = true;
+                }
+
+            }
+        }
+    }
+}
